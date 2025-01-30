bf1220a30a1374335a382b482bfe666cde545ef7
Frank William <williamsp1184@gmail.com>
2025-01-29 21:17:00 -0300

    inclusao de leaderboard

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,42 @@
+'use client'
 import Head from "next/head";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [nortamMarketCap, setNortamMarketCap] = useState(null);
+  const [latamMarketCap, setLatamMarketCap] = useState(null);
+
+  
+  useEffect(() => {
+    // Fetch NORTAM marketCap
+    fetch(
+      "https://springboard.pancakeswap.finance/meme-api/v1/private/token/get/v2?address=0xb3f3c9ec295f6c187b067766f30298c0118ac0df"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        let marketCap = data?.data?.tokenPrice?.marketCap
+        if(marketCap){
+          marketCap = parseFloat(marketCap).toFixed(2)
+        }
+        setNortamMarketCap(marketCap || "N/A");
+      })
+      .catch((error) => console.error("Error fetching NORTAM data:", error));
+
+    // Fetch LATAM marketCap
+    fetch(
+      "https://springboard.pancakeswap.finance/meme-api/v1/private/token/get/v2?address=0xbd366e3805ff2e52c4c1f43835a3ebc7f92786cf"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        let marketCap = data?.data?.tokenPrice?.marketCap
+        if(marketCap){
+          marketCap = parseFloat(marketCap).toFixed(2)
+        }
+        setLatamMarketCap(marketCap || "N/A");
+      })
+      .catch((error) => console.error("Error fetching LATAM data:", error));
+  }, []);
+
   return (
     <>
       <Head>
@@ -77,6 +113,33 @@ export default function Home() {
               className="w-64 h-64 object-contain"
             />
           </div>
+
+          {/* Leaderboard Section */}
+          <section className="bg-gray-800 text-black rounded-lg shadow-lg p-6 mb-10">
+            <h2 className="text-3xl font-bold text-center mb-4 text-white">Leaderboard</h2>
+            <ol className="list-none list-inside"> {/* Remove o marcador padrão da lista */}
+              {[{ name: "NORTAM", marketCap: nortamMarketCap }, { name: "LATAM", marketCap: latamMarketCap }]
+                .sort((a, b) => (b.marketCap || 0) - (a.marketCap || 0)) // Sort by MarketCap in descending order
+                .map((token, index) => (
+                  <li
+                    key={token.name}
+                    className="bg-white my-5 flex items-center rounded-lg shadow-lg p-4 relative"
+                  >
+                    {index === 0 ? (
+                      <span className="text-2xl mr-4">🏆</span> 
+                    ) : (
+                      <span className="text-lg font-bold mr-4">{index + 1}</span>
+                    )}
+                    <div>
+                      <h3 className="text-2xl font-bold">{token.name}</h3>
+                      <p className="text-lg">
+                        MarketCap: {token.marketCap !== null ? `$${token.marketCap}K` : "Loading..."}
+                      </p>
+                    </div>
+                  </li>
+                ))}
+            </ol>
+          </section>
 
         </main>
 
